86f06bc9c89c464db73e0945f0720b58faaa8696
nainaistar <nainaistar@gmail.com>
2020-05-03 14:43:17 +0900

    [Add] next.jsのチュートリアル

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+
+export default function FirstPost() {
+    return (
+        <Layout>
+        <Head>
+        <title>First Post</title>
+    </Head>
+            <h1>First Post</h1>
+            <h2>
+    <Link href="/">
+        <a>
+        リンクタグでBack to home
+    </a>
+    </Link>
+                <br/>
+    <a href="/">
+    aタグでBack to home
+    </a>
+    </h2>
+    </Layout>
+)
+}
